1faddf3b96996bf4d0238833ffbfb58fbfe30a41
fragius <189782341+Fragius@users.noreply.github.com>
2025-03-28 12:40:09 +0000

    Write function getHumanChoice
    Wrote a function getHumanChoice such that
    it will prompt the user to input either
    Rock, Paper or Scissors.

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ function getComputerChoice() {
     const index = Math.floor(Math.random() * 3 - 0.1);
     return choices[index];
 };
+
+function getHumanChoice() {
+    return prompt("Enter either Rock, Paper or Scissors");
+}
