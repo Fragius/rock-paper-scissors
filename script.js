3cfeb320cf225ab9605e3f74df6f85ab425c902d
fragius <189782341+Fragius@users.noreply.github.com>
2025-03-29 08:56:00 +0000

    Create function determineWinner
    Create a function determineWinner such that it
    determines the winner

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-const choices = ["Rock", "Paper", "Scissors"];
+const CHOICES = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
     const index = Math.floor(Math.random() * 3 - 0.1);
-    return choices[index];
+    return CHOICES[index];
 };
 
 function getHumanChoice() {
-    return prompt("Enter either Rock, Paper or Scissors");
+    return prompt("Enter either Rock, paper or scissors");
 }
+
+function determineWinner(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    if (computerChoice === humanChoice) {
+        return "tie";
+    }
+    else if ((humanChoice === "rock" && computerChoice === "paper") || (humanChoice === "paper" && computerChoice === "scissors") || (humanChoice === "scissors" && computerChoice === "rock")) {
+        return "computer";
+    } else if ((humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "paper") || (humanChoice === "rock" && computerChoice === "scissors")) {
+        return "user";
+    } else {
+        return;
+    }
+};
+
