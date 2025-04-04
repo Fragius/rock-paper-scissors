15d5e93b798bf5d04190db5062546f6287219889
fragius <189782341+Fragius@users.noreply.github.com>
2025-04-04 16:22:27 +0300

    Add ui
    Made a user interface using DOM.

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
+let round = 0;
+let computerScore = 0;
+let humanScore =0;
+
 const CHOICES = ["rock", "paper", "scissors"];
+
+const container = document.querySelector(".container");
+const rockBtn = document.createElement("button");
+const paperBtn = document.createElement("button");
+const scissorsBtn = document.createElement("button");
+const gameWinnerDisplay = document.createElement("div");
+container.appendChild(rockBtn);
+container.appendChild(paperBtn);
+container.appendChild(scissorsBtn);
+container.appendChild(gameWinnerDisplay);
+rockBtn.textContent = "Rock";
+paperBtn.textContent = "Paper";
+scissorsBtn.textContent = "Scissors";
+
+const resultsDiv = document.createElement("div");
+container.appendChild(resultsDiv);
+resultsDiv.textContent = "Round 0 Human Score 0 Computer Score 0";
+
+const messageDiv =  document.createElement("div");
+container.appendChild(messageDiv);
+
+console.log(container);
+rockBtn.addEventListener("click", () => playRound("rock"));
+paperBtn.addEventListener("click", () => playRound("paper"));
+scissorsBtn.addEventListener("click", () => playRound("scissors"));
 
 function getComputerChoice() {
     const index = Math.floor(Math.random() * 3 - 0.1);
     return CHOICES[index];
 };
-
-function getHumanChoice() {
-    return prompt("Enter either rock, paper or scissors")
-}
 
 function determineWinner(humanChoice, computerChoice) { 
     if (computerChoice === humanChoice) {
@@ -23,47 +48,45 @@ function determineWinner(humanChoice, computerChoice) {
 };
 
 function createMessage(humanChoice, computerChoice, winner) {
-    if (!winner) {
-        return `invalid input: ${humanChoice} is not a valid input`
-    } else if (winner === "user") {
-        return `You win! ${humanChoice} beats ${computerChoice}.`
-    } else if (winner === "computer") {
-        return `You lose! ${computerChoice} beats ${humanChoice}`
-    } else {
-        return `It's a tie! both players chose ${humanChoice}`
+    switch (winner) {
+        case "user":
+            return `You win! ${humanChoice} beats ${computerChoice}.`
+
+        case "computer":
+            return `You lose! ${computerChoice} beats ${humanChoice}`
+        default:
+            return `It's a tie! both players chose ${humanChoice}`
     }
 }
 
-function playRound(humanChoice, computerChoice) {
-    return winner = determineWinner(humanChoice, computerChoice);
+function playRound(humanChoice) {
+    if (round === 5) {
+        round = 1;
+        humanScore = 0;
+        computerScore = 0;
+    } else {
+    round++;
+    } 
+    const computerChoice = getComputerChoice();
+    const winner = determineWinner(humanChoice, computerChoice);
+    messageDiv.textContent = createMessage(humanChoice, computerChoice, winner);
+    incrementWinner(winner);
+    resultsDiv.textContent = `Round ${round} Human Score ${humanScore} Computer Score ${computerScore}`;
+    if (round === 5) {
+        humanScore === computerScore 
+        ? gameWinnerDisplay.textContent = "It's a tie!" 
+        : (humanScore > computerScore 
+            ? gameWinnerDisplay.textContent = "You win!"
+            : gameWinnerDisplay.textContent = "You lost!" );
+    } else {
+        gameWinnerDisplay.textContent = "";
+    }
 };
 
-function incrementWinner(winner, humanScore, computerScore) {
+function incrementWinner(winner) {
     if (winner === "user") {
         humanScore++;
     } else if (winner === "computer") {
-        computerScore++;
+        computerScore++
     }
-    return [humanScore, computerScore];
 }
-
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-    for (let r = 0; r < 5; r++) {
-        let computerChoice = getComputerChoice();
-        let humanChoice = getHumanChoice().toLowerCase();
-        const winner = playRound(humanChoice, computerChoice);
-        [humanScore, computerScore] = incrementWinner(winner, humanScore, computerScore);
-        alert(createMessage(humanChoice, computerChoice, winner));
-    }
-    if (humanScore > computerScore) {
-        alert("You won!");
-    } else if ( computerScore < humanScore) {
-        alert("You lost!");
-    } else {
-        alert("It's a tie!");
-    }
-};
-
-playGame();
